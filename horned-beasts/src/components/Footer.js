999eef84529f3e3dc71a818b40a336b7b7425c70
NashatAlzaatreh <nashat.alzaatra@yahoo.com>
2021-09-04 23:20:13 +0300

    lab01 Done

---
+++ horned-beasts/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Nashat Alzaatreh &copy; 2021</h2>
+      </div>
+    );
+  }
+}
+
+export default Footer;
